2c0642451dc6439a7654cb8668602c7cbadd0bf4
nkgrnkgr <beefupme@gmail.com>
2023-04-09 20:03:35 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Zen_Kaku_Gothic_New } from "next/font/google";
+
+const ZenKakuGothicNew_normal = Zen_Kaku_Gothic_New({
+  weight: "400",
+  subsets: ["cyrillic"],
+});
+const ZenKakuGothicNew_bold = Zen_Kaku_Gothic_New({
+  weight: "700",
+  subsets: ["cyrillic"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${ZenKakuGothicNew_normal.style.fontFamily},
+            ${ZenKakuGothicNew_bold.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
