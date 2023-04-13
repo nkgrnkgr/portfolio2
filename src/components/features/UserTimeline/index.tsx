ef7fa3dc1f08a3bf773076c25c72c63dfba8380e
taiki-nishi <taiki-nishi@cybozu.co.jp>
2023-04-13 17:50:41 +0900

    ソート順を変更

--- src/components/features/UserTimeline/index.tsx
+++ src/components/features/UserTimeline/index.tsx
@@ -3,14 +3,6 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 type Content = Record<number, Timeline[]>;
 
-const convertToContent = (timelineItems: Timeline[]) =>
-  timelineItems.reduce<Content>((accumulator, currentValue) => {
-    const year = currentValue.date.split("-")[0];
-    const list = accumulator[Number(year)];
-    accumulator[Number(year)] = list ? [...list, currentValue] : [currentValue];
-    return accumulator;
-  }, {});
-
 type Props = {
   timelineItems: Timeline[];
 };
@@ -19,52 +11,62 @@ export const UserTimeline: React.FC<Props> = ({ timelineItems }) => {
   const content = convertToContent(timelineItems);
   return (
     <Box>
-      {Object.entries(content).map(([key, value]) => (
-        <Flex key={key}>
-          <Text fontWeight="extrabold" fontFamily="Helvetica" pr="4">
-            {key}
-          </Text>
-          <Flex direction="column">
-            {value.map((item) => (
-              <Flex
-                direction="column"
-                key={item.title}
-                sx={{
-                  position: "relative",
-                  pl: 4,
-                  _before: {
-                    content: '""',
-                    position: "absolute",
-                    left: "-2px",
-                    top: "10px",
-                    bottom: "0",
-                    width: "2px",
-                    background: "#000",
-                  },
-                }}
-              >
-                <Box
+      {Object.entries(content)
+        .sort((a, b) => Number(b[0]) - Number(a[0]))
+        .map(([key, value]) => (
+          <Flex key={key}>
+            <Text fontWeight="extrabold" fontFamily="Helvetica" pr="4">
+              {key}
+            </Text>
+            <Flex direction="column">
+              {value.map((item) => (
+                <Flex
+                  direction="column"
+                  key={item.title}
                   sx={{
-                    position: "absolute",
-                    display: "flex",
-                    alignItem: "center",
-                    justifyContent: "center",
-                    border: "4px solid #fff",
-                    width: "16px",
-                    height: "16px",
-                    borderRadius: "50%",
-                    top: "5px",
-                    left: "-9px",
-                    backgroundColor: "#000",
+                    position: "relative",
+                    pl: 4,
+                    _before: {
+                      content: '""',
+                      position: "absolute",
+                      left: "-2px",
+                      top: "10px",
+                      bottom: "0",
+                      width: "2px",
+                      background: "#000",
+                    },
                   }}
-                ></Box>
-                <Box>{item.title}</Box>
-                <Box>{item.content}</Box>
-              </Flex>
-            ))}
+                >
+                  <Box
+                    sx={{
+                      position: "absolute",
+                      display: "flex",
+                      alignItem: "center",
+                      justifyContent: "center",
+                      border: "4px solid #fff",
+                      width: "16px",
+                      height: "16px",
+                      borderRadius: "50%",
+                      top: "5px",
+                      left: "-9px",
+                      backgroundColor: "#000",
+                    }}
+                  ></Box>
+                  <Box>{item.title}</Box>
+                  <Box>{item.content}</Box>
+                </Flex>
+              ))}
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
+        ))}
     </Box>
   );
 };
+
+const convertToContent = (timelineItems: Timeline[]) =>
+  timelineItems.reduce<Content>((accumulator, currentValue) => {
+    const year = currentValue.date.split("-")[0];
+    const list = accumulator[Number(year)];
+    accumulator[Number(year)] = list ? [...list, currentValue] : [currentValue];
+    return accumulator;
+  }, {});
