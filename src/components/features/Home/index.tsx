64d8866aaafb6a59ce530c9a642eab4eb5913918
nkgrnkgr <beefupme@gmail.com>
2023-04-16 21:43:29 +0900

    update

--- src/components/features/Home/index.tsx
+++ src/components/features/Home/index.tsx
@@ -1,7 +1,6 @@
-import { ContentWrapper } from "@/components/shared/ContentWrapper";
 import { GithubLanguagesResponse } from "@/types/GithubLanguages";
 import { TimelineContent } from "@/types/Timeline";
-import { Box, Container, Img, SimpleGrid } from "@chakra-ui/react";
+import { Box, Center, Container, Img, SimpleGrid } from "@chakra-ui/react";
 import { GithubGrassGraph } from "../GithubGrassGraph";
 import { Languages } from "../Languages";
 import { UserTimeline } from "../UserTimeline";
@@ -18,7 +17,7 @@ export const Home: React.FC<Props> = ({
 }) => {
   return (
     <Container maxWidth="1200px" mt={6}>
-      <SimpleGrid minChildWidth="300px">
+      <SimpleGrid minChildWidth="300px" gap={4} mb={4}>
         <Box minH="400px">
           <WelcomeMessage />
         </Box>
@@ -26,19 +25,35 @@ export const Home: React.FC<Props> = ({
           <Img src="/icons.svg" alt="Github Grass Graph" />
         </Box>
       </SimpleGrid>
-      <SimpleGrid minChildWidth="300px">
-        <Box p={10}>
-          <Languages githubLanguages={githubLanguages} />
-        </Box>
-        <Box display="flex" alignItems="center">
+      <SimpleGrid minChildWidth="300px" gap={4} mb={4}>
+        <Wrapper>
+          <Box p={10}>
+            <Languages githubLanguages={githubLanguages} />
+          </Box>
+        </Wrapper>
+        <Wrapper>
           <GithubGrassGraph />
-        </Box>
+        </Wrapper>
       </SimpleGrid>
-      <Box mt={4}>
-        <ContentWrapper>
+      <SimpleGrid minChildWidth="300px" gap={4}>
+        <Wrapper>
           <UserTimeline timelineContents={timelineContents} />
-        </ContentWrapper>
-      </Box>
+        </Wrapper>
+      </SimpleGrid>
     </Container>
+  );
+};
+
+const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <Center
+      p={2}
+      background="rgba(255,255,255, 0.2)"
+      boxShadow="0 2rem 5rem -1rem rgba(0,16,36,9%), inset 0 1px rgba(255,255,255,.6)"
+      backdropFilter="blur(4px)"
+      borderRadius={10}
+    >
+      {children}
+    </Center>
   );
 };
